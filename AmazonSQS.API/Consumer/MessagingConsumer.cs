30f03b9645a6b33d8f64495a5f1105f618bd8deb
AlefT94 <alef_teodoro1@hotmail.com>
2025-03-14 13:55:44 -0300

    Add project files.

---
+++ AmazonSQS.API/Consumer/MessagingConsumer.cs
@@ -0,0 +1,65 @@
+﻿
+using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Net;
+
+namespace AmazonSQS.API.Consumer;
+
+public class MessagingConsumer : BackgroundService
+{
+    private readonly AmazonSQSClient _client;
+    private readonly string _queueUrl;
+    public MessagingConsumer()
+    {
+        _client = new AmazonSQSClient(RegionEndpoint.USEast2);
+        _queueUrl = GetQueueUrlAsyncText(_client).Result;
+    }
+
+    private async Task<string> GetQueueUrlAsyncText(AmazonSQSClient client)
+    {
+        var queueResponse = await client.GetQueueUrlAsync("MyTestQueue");
+        return queueResponse.QueueUrl;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var receiveMessageRequest = new ReceiveMessageRequest
+                {
+                    QueueUrl = _queueUrl,
+                    MaxNumberOfMessages = 10,
+                    MessageAttributeNames = new List<string> { "All" }, //Retrieves all message attributes.
+                    WaitTimeSeconds = 5
+                };
+
+                var messages = await _client.ReceiveMessageAsync(receiveMessageRequest, stoppingToken);
+
+                if (messages.HttpStatusCode == HttpStatusCode.OK)
+                {
+                    foreach (var message in messages.Messages)
+                    {
+                        Console.WriteLine($"Message: {message.Body}");
+                        var deleteMessageRequest = new DeleteMessageRequest
+                        {
+                            QueueUrl = _queueUrl,
+                            ReceiptHandle = message.ReceiptHandle
+                        };
+                        await _client.DeleteMessageAsync(deleteMessageRequest);
+                    }
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    
+    }
+}
---
+++ AmazonSQS.API/Controllers/MessagingController.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.SQS.Model;
+using Amazon.SQS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Amazon;
+
+namespace AmazonSQS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagingController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SendMessageAsync([FromBody] string message)
+        {
+            var client = new AmazonSQSClient(RegionEndpoint.USEast2);
+            var queueUrl = await client.GetQueueUrlAsync("MyTestQueue");
+
+            var messageSQS = new SendMessageRequest
+            {
+                //QueueUrl = "https://sqs.us-east-2.amazonaws.com/140023366146/MyTestQueue",
+                QueueUrl = queueUrl.QueueUrl,
+                MessageBody = new { Text = message, Date = DateTime.Now }.ToString()
+            };
+
+            await client.SendMessageAsync(messageSQS);
+
+            return Ok();
+        }
+    }
+}
